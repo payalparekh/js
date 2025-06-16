1c6b41473f738e796e56ade33a38f57cc9822f75
payalparekh <155072547+payalparekh@users.noreply.github.com>
2025-06-16 12:57:01 +0000

    1 basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 144553
+let accountEmail = "payal@google.com"
+var accountPassword ="12345"
+accountCity="Jaipur"
+
+// accountID=2// not allowed
+ accountEmail="ab@gn.com"
+ accountPassword="21213321"
+ accountCity="Bengaluru"
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity])
+
