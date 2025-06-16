2450c20505d6ad2fe4de3325aa1d9a1475373643
payalparekh <155072547+payalparekh@users.noreply.github.com>
2025-06-16 18:32:01 +0000

    datatype summary done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+//Primitive
+
+//7 types: String, Number, Boolean, Null, undefined, symbol, Bigint
+
+
+
+const score= 100
+const scorevalue=100.3
+const isLOggedin= false
+const outsidetemp=null
+
+let useremail;
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+
+console.log(id===anotherid);
+
+const bignumber= 25455845684897546845n
+
+//Reference (Non-primitive)
+
+//Array, Objects, Functions
+
+//array
+const heros=["abc","xyz","pqr"]
+
+//objects
+let myobj={
+    name:"abc",
+    age=20,
+}
