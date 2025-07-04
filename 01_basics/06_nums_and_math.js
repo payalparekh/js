80162b4ec854202d1443bf32c16e964934610dee
payalparekh <155072547+payalparekh@users.noreply.github.com>
2025-07-04 12:53:51 +0000

    nums and maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -8,4 +8,5 @@ console.log(balance.toString());
 //console.log(balance.toString().length);
 console.log(balance.toFixed(2));
 
-const othernumber
+const otherNumber= 23.8966
+console.log(otherNumber.toPrecision(3));
